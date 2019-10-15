25c7ec05efffff36e7cf8f773f67ab9e0bcd8b66
Jordan Faas-Bush <jmanfb@gmail.com>
2019-10-15 18:55:53 -0400

    Created bucket data scriptable object for data storage

---
+++ Assets/Scripts/BucketData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName ="BucketData", menuName = "Create Bucket Data")]
+public class BucketData : ScriptableObject
+{
+    public string bucketName;
+    public int bucketID = -1;
+    public Sprite bucketSprite;
+    [Header("Also put in things like meshes here so that it's easy to find them!")]
+    [Tooltip("I have no clue if we're actually going to use this")]
+    public float bucketRarity = 1;
+}
