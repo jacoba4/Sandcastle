a29f6d0389a69fc915371f05fc3f51c2be87f961
Whiyu <firstlyendlastly@gmail.com>
2019-10-16 14:52:26 -0400

    Added pause menu functionality

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private void Start()
+    {
+        
+    }
     public void PlayGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
