9f817cfd12fb43a6ed01e49a8b46b2869318c7db
Jordan Faas-Bush <jmanfb@gmail.com>
2019-10-22 19:45:29 -0400

    Added seaweed and seashell prefabs

---
+++ Assets/DecorationGun.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecorationGun : MonoBehaviour
+{
+    public bool isSeaWeed = true;
+
+    public void Fire(PlayerControl player)
+    {
+        player.UpgradeFacingBlock(isSeaWeed);
+    }
+}
